3f7b7cc22ae704d23d85dd617087c34e76d9ff96
Edwin Gallardo Ordóñez <edwingallardo516@gmail.com>
2021-06-29 17:28:25 -0500

    1

---
+++ app/Models/Post.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'iframe',
+        'image',
+        'user_id',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function getGetExcerptAttribute()
+    {
+        return substr($this->body,0,140);
+    }
+
+    // Modelo para obtener la url de la imagen
+    public function getGetImageAttribute()
+    {
+        if($this->image)
+            return url("storage/$this->image");
+            // La imagen está en una carpeta privada, por lo cual se necesita permiso 
+            // con el comando php artisan storage:link para tener acceso
+    }
+}
